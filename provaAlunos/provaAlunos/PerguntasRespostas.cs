2d949059a51fd881e3f1b06d96cc9773271bc7e6
AllShad <gabrielmoreira972@gmail.com>
2023-01-12 21:44:01 -0300

    Adição do projeto de verificação de notas de 5 alunos diferentes na mesma prova

---
+++ provaAlunos/provaAlunos/PerguntasRespostas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class PerguntasRespostas 
+{
+	public string[] perguntas = new string[10] { "1.O que são VETORES \n(A) Estruturas de dados Rígido \n(B) Estruturas de dados homogêneas \n(C) Estruturas de dados Restrito",
+                                                "2.Qual é o Paradgma que executam processos como se fossem funções matemáticas \n(A) Paradgma Fundador \n(B) Paradgma Operacional \n(C) Paradgma Funcional", 
+                                                "3.Qual é o Paradgma que possui pontos em comum com a programação estruturada? \n(A) Paradgma Orientação a Objetos \n(B) Paradgma Orientação a Discretos \n(C) Paradgma Orientação a Concretos" ,
+                                                "4.Qual é o Operador que é semelhante ao seu uso na matemática e representam diferentes cálculos? \n(A) Operador Analítico \n(B) Operador Matemático \n(C) Operador Realístico" ,
+                                                "5.Qual é o Operador que estabelece relação entre dados que resultam em valores verdadeiro ou falso? \n(A) Operador Relacional \n(B) Operador Organizacional \n(C) Operador Racional" ,
+                                                "6.Qual é o Operador é utilizado para comparações compostas ? \n(A) Operador Nobre \n(B) Operador Exato \n(C) Operador Lógico" ,
+                                                "7.Qual é o Operador age como se, para que seu resultado seja verdadeiro? \n(A) Operador C \n(B) Operador E \n(C) Operador A" ,
+                                                "8.Qual é o Operador que possui maior facilidade em retornar verdadeiro? \n(A) Operador OU \n(B) Operador AU \n(C) Operador AO" ,
+                                                "9.Qual é o Operador que apenas inverte o valor verdadeiro ou falso contidos em uma estrutura de dados? \n(A) Operador QUÃO \n(B) Operador SÃO \n(C) Operador NÃO" ,
+                                                "10.O que é uma variável? \n(A) É uma constante declaração na seção de comandos. \n(B) É um espaço criado na memória para armazenar dados. \n(C) É um cluster de bits que fica no início do disco." };
+	public string[] respostas = new string[10] {"b","c", "a", "b", "a", "c", "b", "a", "c", "b" };
+	public int[] notaAluno = new int[2];
+	public string[,] matriz = new string[5, 10];
+}
---
+++ provaAlunos/provaAlunos/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Threading;
+
+namespace atividade
+{
+    class ProvaAlunos 
+    {
+        static void matriz() 
+        {
+            PerguntasRespostas perguntasRespostas = new PerguntasRespostas();
+            int incrementação = 0;
+            int acertoAluno = 0;
+            int contPerguntas = 0;
+            
+
+            for (int cont = 0; cont < 5 ; cont++)
+            {
+                Console.Clear();
+                Console.WriteLine("Digite o nome do aluno");
+                perguntasRespostas.matriz[cont, 0] = Convert.ToString(Console.ReadLine());
+
+                for (int cont1 = 1; cont1 < 10 ; cont1++ )
+                {
+                   
+                    Console.WriteLine(perguntasRespostas.perguntas[contPerguntas]);
+                    perguntasRespostas.matriz[cont, cont1] = Convert.ToString(Console.ReadLine());
+
+                    if (perguntasRespostas.matriz[cont, cont1].Equals(perguntasRespostas.respostas[contPerguntas]))
+                    {
+                        acertoAluno++;
+                    }
+                    contPerguntas++;
+                }
+                Console.WriteLine("");
+                perguntasRespostas.notaAluno[cont] = acertoAluno;
+                incrementação++;
+                acertoAluno = 0;
+                contPerguntas = 0;
+            }
+            Menu(perguntasRespostas);
+        }
+
+        static void VizualizarRespostas(PerguntasRespostas perguntasRespostas)
+        {
+            Console.Clear();
+            for (int cont = 0; cont < 5; cont++)
+            {
+                Console.WriteLine(perguntasRespostas.matriz[cont, 0]);
+                for (int cont1 = 1; cont1 < 10; cont1++)
+                {
+                    Console.WriteLine(perguntasRespostas.matriz[cont, cont1]);
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("Digite algo para continuar");
+            Console.ReadLine();
+            Menu(perguntasRespostas);
+        }
+
+        static void VizualizarResultado(PerguntasRespostas perguntasRespostas)
+        {
+            Console.Clear();
+            for (int cont = 0; cont < 5; cont++)
+            {
+                Console.Write($"{perguntasRespostas.matriz[cont, 0]}   {perguntasRespostas.notaAluno[cont]}\n");
+            }
+            Console.WriteLine("Digite algo para continuar");
+            Console.ReadLine();
+            Menu(perguntasRespostas);
+        }
+
+        static void Menu(PerguntasRespostas perguntasRespostas)
+        {
+            Console.Clear();
+            int escolhaMenu;
+            Console.WriteLine("[1]Vizualizar notas\n" +
+                              "[2]Vizualizar respostas dos alunos\n" +
+                              "[3]Sair");
+            escolhaMenu = Convert.ToInt32(Console.ReadLine());
+
+            switch (escolhaMenu)
+            {
+                case 1:
+                    VizualizarResultado(perguntasRespostas);
+                    break;
+                case 2:
+                    VizualizarRespostas(perguntasRespostas);
+                    break;
+                case 3:
+                    Environment.Exit(1);
+                    break;
+                default:
+                    Menu(perguntasRespostas);
+                    break;
+            }
+
+        }
+        static void Main (string[] args)
+        {
+            matriz();
+        }
+    }
+}
