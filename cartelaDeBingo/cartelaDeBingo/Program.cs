064161c9a094e9bd533524f0ffc149e9be164bd3
AllShad <gabrielmoreira972@gmail.com>
2023-01-16 21:00:34 -0300

    adição do projeto de geração de cartela de bingo com numeros aleátorios

---
+++ cartelaDeBingo/cartelaDeBingo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace atividade
+{
+    class cartelaBingo
+    {
+        static void menu(geracartela cartela)
+        {
+            int escolha = 0;
+            Console.Clear();
+            Console.WriteLine("Opções:\n" +
+                              "[1]Grar Cartela\n" +
+                              "[2]Sair");
+            escolha = Convert.ToInt32(Console.ReadLine());
+
+            switch(escolha)
+            {
+                case 1:
+                    cartela.cartela();
+                    menu(cartela);
+                    break;
+                case 2:
+                    Environment.Exit(1);
+                    break;
+                default:
+                    menu(cartela);
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            geracartela cartela = new geracartela();
+            menu(cartela);           
+        }
+    }
+}
---
+++ cartelaDeBingo/cartelaDeBingo/classe/geraCartela.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+public class geracartela
+{
+	public void cartela()
+	{
+		int[,] cartelaBingo = new int[3, 3];
+		Random numeroAleatorio = new Random();
+
+        for (int cont = 0; cont < 3; cont++)
+		{
+			cartelaBingo[cont, 0] = numeroAleatorio.Next(1, 99);
+			for (int cont1 = 1; cont1 < 3; cont1++)
+			{
+				cartelaBingo[cont, cont1] = numeroAleatorio.Next(1, 99);
+            }
+			Console.WriteLine("");
+		}
+
+		for(int cont = 0; cont < 3; cont++)
+		{
+			for(int cont1 = 0; cont1 < 3; cont1++)
+			{
+				if (cartelaBingo[cont, 0].Equals(cartelaBingo[cont, cont1]))
+				{
+					cartelaBingo[cont, 0] = numeroAleatorio.Next(1,99);
+				}
+
+                if (cartelaBingo[0, cont1].Equals(cartelaBingo[cont, cont1]))
+                {
+                    cartelaBingo[0, cont1] = numeroAleatorio.Next(1, 99);
+                }
+            }
+		}
+
+        for (int cont = 0; cont < 3; cont++)
+        {
+            Console.Write($" | {cartelaBingo[cont, 0]} |");
+            Thread.Sleep(100);
+            for (int cont1 = 1; cont1 < 3; cont1++)
+            {              
+                Console.Write($" | {cartelaBingo[cont, cont1]} |");
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("");
+        }
+		Console.WriteLine("Digite algo para continuar");
+		Console.ReadLine();
+    }
+}
